864cfd6a15c8fa8c73a0742812cf73f5477d14d8
viniciusgit2 <4xv6patelfin@gmail.com>
2024-04-16 11:44:31 -0300

    quase feito

---
+++ src/pages/componentes/Porta2.jsx
@@ -0,0 +1,4 @@
+export default function  Porta2() {
+    return (<div className='area'><div className={'selecionada'}><div className={"porta"}> <button className="maçaneta"></button></div></div></div>)
+    
+  }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import Presente from './componentes/Presente'
 import Porta from './componentes/Porta'
+import Porta2 from '../pages/componentes/Porta2'
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <div>
- 
+ <Porta2/>
+ <Porta2/><Porta2/><Porta2/><Porta2/><Porta2/><Porta2/><Porta2/>
  <Porta/>
       </div>
    
