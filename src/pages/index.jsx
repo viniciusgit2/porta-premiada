86098f4f878df34e51988dbfcb31c1ea91165742
viniciusgit2 <4xv6patelfin@gmail.com>
2024-04-16 10:21:06 -0300

    porta-premiada

---
+++ src/pages/index.jsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import Presente from './componentes/Presente'
+import Porta from './componentes/Porta'
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <div>
+ 
+ <Porta/>
+      </div>
+   
+  )
+}
